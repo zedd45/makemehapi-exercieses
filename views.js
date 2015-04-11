e892494f915a9fa12b716cee9b2061c5d9ca2873
Chris Keen <christopher.keen@gmail.com>
2015-04-10 22:40:42 -0700

    exercise 5

---
+++ views.js
@@ -0,0 +1,31 @@
+var Hapi = require('hapi');
+var path = require('path');
+var joi = require('joi');
+
+var server = new Hapi.Server();
+
+server.connection({
+    host: 'localhost',
+    port: Number(process.argv[2]) || 8080,
+});
+
+server.route({
+    path: "/",
+    method: "GET",
+    // The provided answer doesn't actually work here
+    // `handler: { view: 'index.html' }`
+    // perhaps older version(s) of Hapi did inject the query params as global context,
+    // but this didn't provide a context to Handlebars
+    handler: function (request, reply) {
+        reply.view('index.html', { name: request.query.name});
+    }
+});
+
+server.views({
+    engines: {
+        "html": require('handlebars')
+    },
+    path: path.join(__dirname, 'templates')
+});
+
+server.start();
